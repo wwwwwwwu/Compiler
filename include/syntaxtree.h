b603e18b713261f34e9c5986a41d4a52eb7732ac
wuyinan <345476463@qq.com>
2018-05-06 01:45:29 -0700

    debug float and print syntaxtree

--- include/syntaxtree.h
+++ include/syntaxtree.h
@@ -19,16 +19,90 @@ typedef struct syntax_tree_node {
 }syntax_node;
 
 
-syntax_node* init_syntax_node(int,char[]);
-
-syntax_node* init_syntax_node(int lineno,char symbol[]) {
+syntax_node* init_syntax_node(int,char[],char*,...);
+syntax_node* init_syntax_floatnode(int,char[],float);
+syntax_node* init_syntax_child_node(char[],int,...);
+void print_tree(syntax_node*,int,int);
+syntax_node* init_syntax_node(int lineno,char symbol[],char* text,...) {
 	syntax_node* p=malloc(sizeof(syntax_node));
 	strcpy(p->symbol,symbol);
 	p->lineno=lineno;
 	p->is_terminal=1;
 	p->int_val=0;
 	p->nr_child=0;
+/*
+	va_list valist;
+	va_start(valist,symbol);
+	char* text=va_arg(valist,char*);
+	va_end(valist);
+	strcpy(p->inf,text);
+	printf("%s\n",text);
+*/
+	if(text=="INT"){
+		va_list valist;
+		va_start(valist,text);
+		p->int_val=va_arg(valist,int);
+		printf("int a %d",p->int_val);
+		va_end(valist);
+		printf("int b %d",p->int_val);
+	}
+	else if(text!=NULL){
+		strcpy(p->inf,text);
+		printf("%s\n",text);
+	}
+	
 	return p;
 }
 
+syntax_node* init_syntax_floatnode(int lineno,char symbol[],float value) {
+	syntax_node* p=malloc(sizeof(syntax_node));
+	strcpy(p->symbol,symbol);
+	p->lineno=lineno;
+	p->is_terminal=1;
+	p->nr_child=0;
+	p->float_val=value;
+	return p;
+}
+
+syntax_node* init_syntax_child_node(char symbol[],int nr_child,...) {
+	syntax_node* p=malloc(sizeof(syntax_node));
+	strcpy(p->symbol,symbol);
+	p->is_terminal=0;
+	va_list ap;
+	va_start(ap,nr_child);
+	int i;
+	for (i=0;i<nr_child;i++)
+	{
+		syntax_node* q=va_arg(ap,syntax_node*);
+		p->child[i]=q;
+	}
+	return p;
+}
+
+void print_tree(syntax_node *p,int nr_blank,int noerror) {
+	if (noerror==0)
+		return;
+	int i=0;
+	if (p->is_terminal==0&&p->nr_child==0)
+		return;
+	for (;i<nr_blank;i++)
+	{
+		printf("  ");
+	}
+	printf("%s",p->symbol);
+	if ((strcmp(p->symbol,"TYPE")==0)||(strcmp(p->symbol,"ID")==0))
+		printf(": %s",p->inf);
+	else if (strcmp(p->symbol,"INT")==0)
+		printf(": %d",p->int_val);
+	else if (strcmp(p->symbol,"FLOAT")==0)
+		printf(": %f",p->float_val);
+	if (p->is_terminal==0)
+		printf(" (%d)",p->lineno);	
+	printf("\n");
+	int n=0;
+	for (;n<p->nr_child;n++)
+	{
+		print_tree(p->child[n],nr_blank+1,1);
+	}
+}
 #endif
